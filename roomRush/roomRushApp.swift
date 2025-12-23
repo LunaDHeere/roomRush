a50d6aaf89fbc2f1c1ffdaa6384353d589021eef
LunaDHeere <luna.dheere@gmail.com>
2025-12-23 16:55:37 +0100

    ADD: added firebase connection

--- roomRush/roomRushApp.swift
+++ roomRush/roomRushApp.swift
@@ -7,10 +7,21 @@
 
 import SwiftUI
 import CoreData
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct roomRushApp: App {
     let persistenceController = PersistenceController.shared
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
